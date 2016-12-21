d76c13f1bb0d3d10873c3a38a7bb6a147746b388
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2016-12-21 11:39:48 -0700

    Implemented GET all products

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const app = express()
 app.get('/', function(req, res) {
   res.send("Hello World");
 });
+
+app.get('/api/products', (req, res) => {
+  let products = [
+    { name: "Javascript Book", price: 9.99},
+    { name: "A Christmas Carol", price: 19.99}
+  ]
+  let stringified = JSON.stringify(products)
+  res.send(stringified);
+});
+
 app.listen(3000, function() {
   console.log("Listening on port 3000. Go to http://localhost:3000/")
 })
