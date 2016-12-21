06be441d3774a30fea8821c07c71c80892a2d23b
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2016-12-21 11:58:47 -0700

    Added ID

--- app.js
+++ app.js
@@ -6,14 +6,22 @@ app.get('/', function(req, res) {
   res.send("Hello World");
 });
 
+let products = [
+  { id: 1, name: "Javascript Book", price: 9.99},
+  { id: 2, name: "A Christmas Carol", price: 19.99}
+]
+
 app.get('/api/products', (req, res) => {
-  let products = [
-    { name: "Javascript Book", price: 9.99},
-    { name: "A Christmas Carol", price: 19.99}
-  ]
   let stringified = JSON.stringify(products)
   res.send(stringified);
 });
+
+app.get('/api/products/:id', (req, res) => {
+  let id = Number(req.params.id)
+  let product = products.find( (element) => element.id === id);
+  let stringified = JSON.stringify(product);
+  res.send(stringified);
+})
 
 app.listen(3000, function() {
   console.log("Listening on port 3000. Go to http://localhost:3000/")
